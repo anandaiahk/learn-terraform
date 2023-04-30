d02aa1740e385ca9fdab51ea7d0fa6d6f0cd98ab
anandk <kanandaiah9@gmail.com>
2023-04-30 17:59:37 +0530

    declare variables

--- var.tf
+++ var.tf
@@ -9,3 +9,20 @@ output "sample_string" {
 output "sample_string1" {
   value = "var ofsample_string= ${var.sample_string}"
 }
+variable "sample_number" {
+  default = 100
+}
+variable "sample_boolean" {
+  default = true
+}
+variable "sample_list" {
+  default = [
+    100,
+    "anand"
+    true
+    123
+  ]
+}
+output "sample_list" {
+  value = var.sample_list[1]
+}
