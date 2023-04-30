8dbf1a11827cf2912e356500b38f7abebb8c49f2
anandk <kanandaiah9@gmail.com>
2023-04-30 17:19:47 +0530

    declare variables

--- var.tf
+++ var.tf
@@ -7,5 +7,5 @@ output "sample_string" {
   value = var.sample_string
 }
 output "sample_string1" {
-  value = "var ofsample_string= $(var.sample_string)"
+  value = "var ofsample_string= ${var.sample_string}"
 }
