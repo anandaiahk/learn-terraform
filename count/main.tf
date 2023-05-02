9a8ba4b14ce7205f853591641cbe4604e302e048
anandk <kanandaiah9@gmail.com>
2023-05-02 21:41:12 +0530

    declare variables

--- count/main.tf
+++ count/main.tf
@@ -5,6 +5,9 @@ resource "null_resource" "fruits" {
   count = length(var.fruits)
 }
 
+provisioner "local-exec" {
+  command = "echo fruit name - ${var.fruits[count.index]}"
+}
 variable "fruits" {
   default = [ "apple", "banana", "mango"]
 
