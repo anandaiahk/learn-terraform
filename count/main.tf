5d18949c8a7682986af97539e4c7c337bec36dc5
anandk <kanandaiah9@gmail.com>
2023-05-03 19:04:32 +0530

    declare variables

--- count/main.tf
+++ count/main.tf
@@ -1,15 +1,28 @@
-resource "null_resource" "null" {
-  count = 10
-}
+
 resource "null_resource" "fruits" {
-  count = length(var.fruits)
+
+  for_each = var.fruits
 
   provisioner "local-exec" {
-    command = "echo fruit name - ${var.fruits[count.index]}"
+    command = "echo fruit name - ${var.fruits[each.key]}"
+   // command = "echo fruit name - ${var.fruits[count.index]}"
   }
 
 }
 variable "fruits" {
-  default = [ "apple", "banana", "mango"]
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
+    orange = {
+      name = "orange"
+      count = 200
+    }
+    banana = {
+      name = "babana"
+      count = 134
+    }
+  }
 
 }
